54365526218cf2aacb48dac5f16e7fbecaf9862f
MibrahimAcici <ibrahimacici@gmail.com>
2022-08-07 00:56:10 +0300

    datatable

--- app/src/main/java/com/diyabet/diyabetgunlugum/view/fragment/DataTableFragment.kt
+++ app/src/main/java/com/diyabet/diyabetgunlugum/view/fragment/DataTableFragment.kt
@@ -28,13 +28,13 @@ class DataTableFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
 
-        dataList.add(Data("12:00","120","15","1","2"))
-        dataList.add(Data("13:00","110","12","2","3"))
-        dataList.add(Data("13:30","122","11","1","2"))
-        dataList.add(Data("15:00","144","12","1","3"))
-        dataList.add(Data("19:50","125","10","1","2"))
-        dataList.add(Data("20:04","116","17","1","3"))
-        dataList.add(Data("23:00","137","20","1","2"))
+        dataList.add(Data("12:00","120","+15","1","2"))
+        dataList.add(Data("13:00","110","+12","2","3"))
+        dataList.add(Data("13:30","122","+11","1","2"))
+        dataList.add(Data("15:00","144","+12","1","3"))
+        dataList.add(Data("19:50","125","+10","1","2"))
+        dataList.add(Data("20:04","116","+17","1","3"))
+        dataList.add(Data("23:00","137","+20","1","2"))
 
         initAdapter()
         fetchData()
